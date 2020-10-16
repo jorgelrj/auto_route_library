69615dcea776de7f9d41285bd671f084a069b1f9
Jorge <jr.luisrodrigues@gmail.com>
2020-10-16 09:48:00 -0300

    fix: Navigator key on null

--- auto_route/lib/src/extended_navigator.dart
+++ auto_route/lib/src/extended_navigator.dart
@@ -34,7 +34,7 @@ class ExtendedNavigator<T extends RouterBase> extends StatefulWidget {
   }) =>
       (context, nav) {
         var extendedNav = ExtendedNavigator<T>(
-          key: GlobalObjectKey(nav.key),
+          key: GlobalObjectKey(nav?.key),
           navigatorKey: navigatorKey,
           observers: observers,
           onUnknownRoute: onUnknownRoute,
